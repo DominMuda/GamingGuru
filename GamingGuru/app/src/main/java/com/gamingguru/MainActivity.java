aad5f9151ea15cb7306ab4cc60201841f1337e77
Domingo Muñoz Daza <dmunnoz96@gmail.com>
2016-07-05 22:38:07 +0200

    Correción de librería xstream-1.4.9.jar para Android

--- GamingGuru/app/src/main/java/com/gamingguru/MainActivity.java
+++ GamingGuru/app/src/main/java/com/gamingguru/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        cd 
+
         btn = (Button)findViewById(R.id.btn);
 
         btn.setOnClickListener(new View.OnClickListener() {
